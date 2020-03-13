84a15642cf76c42e61fbd70db515df675c6651e8
Olga Kushch <olga-kushch@rambler.ru>
2020-03-13 18:02:43 +0300

    add all files

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function () {
+	$(window).scroll(function() {
+	    $('#mail .section-title').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInLeft");
+	        }
+	    });
+	});
+  $(window).scroll(function() {
+	    $('.skill-trip').each(function(){
+	        var imagePos = $(this).offset().top;
+
+	        var topOfWindow = $(window).scrollTop();
+	        if (imagePos < topOfWindow+650) {
+	            $(this).addClass("fadeInUp");
+	        }
+	    });
+	});
+})
